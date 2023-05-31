5e743f219fa57f9756729717cdac1b813d61315f
ANUPAMASHETTY12 <ampushetty2004@gmail.com>
2023-05-31 12:41:39 +0530

    Autostand list page

---
+++ autostand-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Autostand from '../src/Component/Near-Autostand/Autostand'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Autostand></Autostand>
+      </div>
+    </div>
+  );
+}
+
+export default App;
